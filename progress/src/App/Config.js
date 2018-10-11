8b727778664daee351272a346fb9cdddae357fee
mluizza89 <39567273+mluizza89@users.noreply.github.com>
2018-10-11 16:04:12 -0400

    Update Config.js

--- progress/src/App/Config.js
+++ progress/src/App/Config.js
@@ -2,14 +2,14 @@ let config;
 if (!process.env.NODE_ENV || process.env.NODE_ENV === 'development') {
   // dev code
   config = {
-    app_client_id: 'your_client_id',
+    app_client_id: 'a46171a4d02cb22abf60cadf70a31fa3',
     api_url: "https://accounts.livechatinc.com/",
     serverUrl: 'https://us-central1-livechat-experiments.cloudfunctions.net/restApi',
   }
 } else {
   // production code
   config = {
-    app_client_id: 'your_client_id',
+    app_client_id: 'a46171a4d02cb22abf60cadf70a31fa3',
     api_url: "https://accounts.livechatinc.com/",
     serverUrl: 'https://us-central1-livechat-experiments.cloudfunctions.net/restApi',
   }
